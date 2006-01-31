c5ea8ce9aad6cb4fec1be15359215923b6fa2fea
dockes <none@none>
2006-01-31 11:39:12 +0000

    close/reopen db by default: let us see results of recollindex -i

--- src/qtgui/recoll.h
+++ src/qtgui/recoll.h
@@ -16,7 +16,7 @@
  */
 #ifndef _RECOLL_H_INCLUDED_
 #define _RECOLL_H_INCLUDED_
-/* @(#$Id: recoll.h,v 1.14 2006-01-30 11:15:28 dockes Exp $  (C) 2004 J.F.Dockes */
+/* @(#$Id: recoll.h,v 1.15 2006-01-31 11:39:12 dockes Exp $  (C) 2004 J.F.Dockes */
 #include <string>
 
 #include "rclconfig.h"
@@ -24,7 +24,9 @@
 #include "idxthread.h"
 
 // Misc declarations in need of sharing between the UI files
-extern bool maybeOpenDb(std::string &reason, bool force = false);
+
+// Open the database if needed. We now force a close/open by default
+extern bool maybeOpenDb(std::string &reason, bool force = true);
 
 extern RclConfig *rclconfig;
 extern Rcl::Db *rcldb;
