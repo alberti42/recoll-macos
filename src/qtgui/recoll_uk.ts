b6b76fe237d8f6043f745de2e13c764282ce0631
dockes <none@none>
2006-03-31 07:51:58 +0000

    *** empty log message ***

---
+++ src/qtgui/recoll_uk.ts
@@ -0,0 +1,560 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Configuration problem: </source>
+        <translation>Проблема конфіґурації: </translation>
+    </message>
+    <message>
+        <source>No db directory in configuration</source>
+        <translation>В конфігурації немає каталогу БД </translation>
+    </message>
+    <message>
+        <source>Could not open database in </source>
+        <translation>Не можу відкрити базу даних в </translation>
+    </message>
+    <message>
+        <source>.
+Click Cancel if you want to edit the configuration file before indexation starts, or Ok to let it proceed.</source>
+        <translation>.
+Натисніть Відміна, якщо бажаєте відредагувати конфіґурацію до початку індексування,
+чи OK для продовження.</translation>
+    </message>
+</context>
+<context>
+    <name>Preview</name>
+    <message>
+        <source>Preview</source>
+        <translation>Перегляд</translation>
+    </message>
+    <message>
+        <source>Preview 1</source>
+        <translation>Перегляд 1</translation>
+    </message>
+    <message>
+        <source>&amp;Search for:</source>
+        <translation>&amp;Шукати:</translation>
+    </message>
+    <message>
+        <source>&amp;Next</source>
+        <translation>&amp;Наступне</translation>
+    </message>
+    <message>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <source>&amp;Previous</source>
+        <translation>&amp;Попереднє</translation>
+    </message>
+    <message>
+        <source>Alt+P</source>
+        <translation>Alt+P</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Стерти</translation>
+    </message>
+    <message>
+        <source>Match &amp;Case</source>
+        <translation>&amp;Чутливість до реєстру</translation>
+    </message>
+    <message>
+        <source>Alt+C</source>
+        <translation>Alt+C</translation>
+    </message>
+    <message>
+        <source>Cannot create temporary directory</source>
+        <translation>Не можу створити тимчасову теку</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Відмінити</translation>
+    </message>
+    <message>
+        <source>Can&apos;t turn doc into internal rep for </source>
+        <translation>Не можу перетворити документ у внутрішній вигляд </translation>
+    </message>
+    <message>
+        <source>Creating preview text</source>
+        <translation>Створюю текст для перегляду</translation>
+    </message>
+    <message>
+        <source>Loading preview text into editor</source>
+        <translation>Завантажую текст перегляду в редактор</translation>
+    </message>
+</context>
+<context>
+    <name>RclMain</name>
+    <message>
+        <source>Indexing in progress: </source>
+        <translation>Індексується: </translation>
+    </message>
+    <message>
+        <source>Query results</source>
+        <translation>Query results</translation>
+    </message>
+    <message>
+        <source>Query results (sorted)</source>
+        <translation>Результати запиту (сортовано)</translation>
+    </message>
+    <message>
+        <source>Advanced search</source>
+        <translation>Складний пошук</translation>
+    </message>
+    <message>
+        <source>Sort criteria</source>
+        <translation>Критерії пошуку</translation>
+    </message>
+    <message>
+        <source>User interface preferences</source>
+        <translation>Налаштування інтерфейсу</translation>
+    </message>
+    <message>
+        <source>Cannot retrieve document info from database</source>
+        <translation>Не можу здобути документ з бази даних</translation>
+    </message>
+    <message>
+        <source>Cannot access document file: </source>
+        <translation>Не можу прочитати файл: </translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Перегляд</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Попередження</translation>
+    </message>
+    <message>
+        <source>Can&apos;t create preview window</source>
+        <translation>Не можу створити вікно перегляду</translation>
+    </message>
+    <message>
+        <source>No external viewer configured for mime type </source>
+        <translation>Не знаю переглядача для mime-типа </translation>
+    </message>
+    <message>
+        <source>Executing: [</source>
+        <translation>Виконую: [</translation>
+    </message>
+    <message>
+        <source>About Recoll</source>
+        <translation>Про Recoll</translation>
+    </message>
+    <message>
+        <source>Starting help browser </source>
+        <translation>Запускаю перегляд довідки </translation>
+    </message>
+    <message>
+        <source>Document history</source>
+        <translation>Історія документів</translation>
+    </message>
+    <message>
+        <source>Document history (sorted)</source>
+        <translation>Історія документів (сортована)</translation>
+    </message>
+    <message>
+        <source>History data</source>
+        <translation>Дані історії</translation>
+    </message>
+    <message>
+        <source>Query details</source>
+        <translation>Деталі запиту</translation>
+    </message>
+</context>
+<context>
+    <name>RclMainBase</name>
+    <message>
+        <source>Recoll</source>
+        <translation>Recoll</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <source>&amp;Tools</source>
+        <translation>&amp;Інструменти</translation>
+    </message>
+    <message>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Налаштування</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Довідка</translation>
+    </message>
+    <message>
+        <source>Search tools</source>
+        <translation>Інструменти пошуку</translation>
+    </message>
+    <message>
+        <source>Result list</source>
+        <translation>Список результатів</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Вихід</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>&amp;Вихід</translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <source>Start Indexing</source>
+        <translation>Почати індексування</translation>
+    </message>
+    <message>
+        <source>Start &amp;Indexing</source>
+        <translation>&amp;Почати індексування</translation>
+    </message>
+    <message>
+        <source>About Recoll</source>
+        <translation>Про Recoll</translation>
+    </message>
+    <message>
+        <source>&amp;About Recoll</source>
+        <translation>Про &amp;Recoll</translation>
+    </message>
+    <message>
+        <source>User manual</source>
+        <translation>Довідник користувача</translation>
+    </message>
+    <message>
+        <source>&amp;User manual</source>
+        <translation>&amp;Довідник користувача</translation>
+    </message>
+    <message>
+        <source>Document &amp;History</source>
+        <translation>&amp;Історія документів</translation>
+    </message>
+    <message>
+        <source>Document  History</source>
+        <translation>Історія документів</translation>
+    </message>
+    <message>
+        <source>Advanced Search</source>
+        <translation>Складний пошук</translation>
+    </message>
+    <message>
+        <source>&amp;Advanced Search</source>
+        <translation>&amp;Складний пошук</translation>
+    </message>
+    <message>
+        <source>Advanced/complex  Search</source>
+        <translation>Складний (поглиблений) пошук</translation>
+    </message>
+    <message>
+        <source>&amp;Sort parameters</source>
+        <translation>Параметри &amp;сортування</translation>
+    </message>
+    <message>
+        <source>Sort parameters</source>
+        <translation>Параметри сортування</translation>
+    </message>
+    <message>
+        <source>Next page</source>
+        <translation>Наступна сторінка</translation>
+    </message>
+    <message>
+        <source>Next page of results</source>
+        <translation>Наступна сторінка результатів</translation>
+    </message>
+    <message>
+        <source>Previous page</source>
+        <translation>Попередня сторінка</translation>
+    </message>
+    <message>
+        <source>Previous page of results</source>
+        <translation>Попередня сторінка результатів</translation>
+    </message>
+    <message>
+        <source>&amp;Query configuration</source>
+        <translation>&amp;Конфіґурація запиту</translation>
+    </message>
+</context>
+<context>
+    <name>RclResList</name>
+    <message>
+        <source>Result list</source>
+        <translation>Список результатів</translation>
+    </message>
+    <message>
+        <source>Unavailable document</source>
+        <translation>Документ недосяжний</translation>
+    </message>
+    <message>
+        <source>Show query details</source>
+        <translation>Показати запит</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Displaying results starting at index %1 (maximum set size %2)&lt;/b&gt;&lt;/p&gt;
+</source>
+        <translation>&lt;b&gt;Результати починаючи з %1 (максимально %2)&lt;/b&gt;&lt;/p&gt;
+</translation>
+    </message>
+    <message>
+        <source>&amp;Preview</source>
+        <translation>&amp;Переглянути</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Редагувати</translation>
+    </message>
+    <message>
+        <source>&amp;Copy File Name</source>
+        <translation>&amp;Копіювати ім&apos;я файлу</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Url</source>
+        <translation>Копіювати &amp;Url</translation>
+    </message>
+</context>
+<context>
+    <name>SSearchBase</name>
+    <message>
+        <source>SSearchBase</source>
+        <translation>SSearchBase</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Стерти</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <source>Erase search entry</source>
+        <translation>Стерти вміст рядка запита</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Знайти</translation>
+    </message>
+    <message>
+        <source>Start query</source>
+        <translation>Почати запит</translation>
+    </message>
+    <message>
+        <source>Choose type of search: any term, all terms, filename with possible wildcards.</source>
+        <translation>Оберіть тип запиту: будь-яке слово, усі слова, ім&apos;я файлу (можливі шаблони).</translation>
+    </message>
+    <message>
+        <source>Enter search terms here</source>
+        <translation>Ключові слова</translation>
+    </message>
+    <message>
+        <source>Any term</source>
+        <translation>Будь-яке слово</translation>
+    </message>
+    <message>
+        <source>All terms</source>
+        <translation>Усі слова</translation>
+    </message>
+    <message>
+        <source>File name</source>
+        <translation>Ім&apos;я файлу</translation>
+    </message>
+</context>
+<context>
+    <name>SortForm</name>
+    <message>
+        <source>Sort Criteria</source>
+        <translation>Критерії сортування</translation>
+    </message>
+    <message>
+        <source>Sort the</source>
+        <translation>Сортувати</translation>
+    </message>
+    <message>
+        <source>most relevant results by:</source>
+        <translation>кращі результати за:</translation>
+    </message>
+    <message>
+        <source>Descending</source>
+        <translation>Спаданням</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Скинути</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Закрити</translation>
+    </message>
+</context>
+<context>
+    <name>UIPrefsDialog</name>
+    <message>
+        <source>MyDialog</source>
+        <translation>MyDialog</translation>
+    </message>
+    <message>
+        <source>User interface</source>
+        <translation>Інтерфейс</translation>
+    </message>
+    <message>
+        <source>Number of entries in a result page</source>
+        <translation>Кількість результатів на сторінку</translation>
+    </message>
+    <message>
+        <source>Result list font</source>
+        <translation>Шрифт списку результатів</translation>
+    </message>
+    <message>
+        <source>Helvetica-10</source>
+        <translation>Helvetica-10</translation>
+    </message>
+    <message>
+        <source>Opens a dialog to select the result list font</source>
+        <translation>Відкриває діалог вибору шрифту списку результатів</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Скинути</translation>
+    </message>
+    <message>
+        <source>Resets the result list font to the system default</source>
+        <translation>Повертає шрифт у типовий системний</translation>
+    </message>
+    <message>
+        <source>HTML help browser</source>
+        <translation>Переглядач довідки у HTML</translation>
+    </message>
+    <message>
+        <source>Select file</source>
+        <translation>Оберіть файл</translation>
+    </message>
+    <message>
+        <source>Show document type icons in result list</source>
+        <translation>Відображати типи документів у списку результатів</translation>
+    </message>
+    <message>
+        <source>Search parameters</source>
+        <translation>Параметри пошуку</translation>
+    </message>
+    <message>
+        <source>Stemming language</source>
+        <translation>Мова словоутворення</translation>
+    </message>
+    <message>
+        <source>Dynamically build abstracts</source>
+        <translation>Динамічно будувати реферати</translation>
+    </message>
+    <message>
+        <source>Do we try to build abstracts for result list entries by using the context of query terms ? 
+May be slow for big documents.</source>
+        <translation>Чи намагатися будувати реферати для результатів пошуку, використовуючі контекст знайдених слів?
+Може працювати повільно для великих документів.</translation>
+    </message>
+    <message>
+        <source>Replace abstracts from documents</source>
+        <translation>Заміняти реферати з документів</translation>
+    </message>
+    <message>
+        <source>Do we synthetize an abstract even if the document seemed to have one?</source>
+        <translation>Чи робити новий реферат, навіть якщо якийсь вже є в документі?</translation>
+    </message>
+    <message>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <source>Apply changes</source>
+        <translation>Застосувати зміни</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Відміна</translation>
+    </message>
+    <message>
+        <source>Discard changes</source>
+        <translation>Відмінити зміни</translation>
+    </message>
+</context>
+<context>
+    <name>advsearch</name>
+    <message>
+        <source>Advanced search</source>
+        <translation>Складний пошук</translation>
+    </message>
+    <message>
+        <source>Search for files&lt;br&gt;having all of:</source>
+        <translation>Шукати файли,&lt;br&gt;в яких є все з:</translation>
+    </message>
+    <message>
+        <source>All non blank fields will be combined with AND conjunctions</source>
+        <translation>Всі непусті поля буде об&apos;єднано за допомогою AND</translation>
+    </message>
+    <message>
+        <source>Any of these words</source>
+        <translation>Будь-яке з цих слів</translation>
+    </message>
+    <message>
+        <source>File name matching</source>
+        <translation>Пошук по іменах файлів</translation>
+    </message>
+    <message>
+        <source>None of these words</source>
+        <translation>Жодне з цих слів</translation>
+    </message>
+    <message>
+        <source>All these words</source>
+        <translation>Всі ці слова</translation>
+    </message>
+    <message>
+        <source>This exact phrase</source>
+        <translation>Точна фраза</translation>
+    </message>
+    <message>
+        <source>Restrict file types</source>
+        <translation>Обмежити типи файлів</translation>
+    </message>
+    <message>
+        <source>Searched file types</source>
+        <translation>Шукати в файлах типу</translation>
+    </message>
+    <message>
+        <source>All ----&gt;</source>
+        <translation>Всі -----&gt;</translation>
+    </message>
+    <message>
+        <source>Sel -----&gt;</source>
+        <translation>Виб -----&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;----- Sel</source>
+        <translation>&lt;----- Виб</translation>
+    </message>
+    <message>
+        <source>&lt;----- All</source>
+        <translation>&lt;----- Всі</translation>
+    </message>
+    <message>
+        <source>Ignored file types</source>
+        <translation>Ігнорувати файли типу</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>Перегляд</translation>
+    </message>
+    <message>
+        <source>Restrict results to files in subtree:</source>
+        <translation>Обмежити пошук по файлах з піддерева:</translation>
+    </message>
+    <message>
+        <source>Start Search</source>
+        <translation>Шукати</translation>
+    </message>
+    <message>
+        <source>Dismiss</source>
+        <translation>Відмінити</translation>
+    </message>
+</context>
+</TS>
