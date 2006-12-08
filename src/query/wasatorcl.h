c5463d4b42d8b1dfb837fdf21a0787b3b1ca436b
dockes <none@none>
2006-12-08 17:18:34 +0000

    1st query

--- src/query/wasatorcl.h
+++ src/query/wasatorcl.h
@@ -1,6 +1,9 @@
 #ifndef _WASATORCL_H_INCLUDED_
 #define _WASATORCL_H_INCLUDED_
-/* @(#$Id: wasatorcl.h,v 1.1 2006-11-30 18:12:16 dockes Exp $  (C) 2006 J.F.Dockes */
+/* @(#$Id: wasatorcl.h,v 1.2 2006-12-08 17:18:34 dockes Exp $  (C) 2006 J.F.Dockes */
+
+#include "rcldb.h"
+#include "searchdata.h"
 
 extern Rcl::SearchData *wasatorcl(WasaQuery *wasa);
 
