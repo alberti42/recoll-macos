1dd66b5b1df44c99ce5b34a444df6f6f41c0e9d8
dockes <none@none>
2008-07-28 10:20:20 +0000

    *** empty log message ***

---
+++ src/utils/fileudi.h
@@ -0,0 +1,33 @@
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#ifndef _FILEUDI_H_INCLUDED_
+#define _FILEUDI_H_INCLUDED_
+/* @(#$Id: fileudi.h,v 1.1 2008-07-28 10:20:20 dockes Exp $  (C) 2004 J.F.Dockes */
+#include <string>
+using std::string;
+
+// Unique Document Ids for the file-based indexer (main Recoll
+// indexer).  Document Ids are built from a concatenation of the file
+// path and the internal path (ie: email number inside
+// folder/attachment number/etc.)  As the size of Xapian terms is
+// limited, the path is truncated to a maximum length, and completed
+// by a hash of the remainder. So the unique id looks like:
+// /some/truncated/paHASHVALUE|ipath
+
+extern void make_udi(const string& fn, const string& ipath, string &udi);
+
+#endif /* _FILEUDI_H_INCLUDED_ */
