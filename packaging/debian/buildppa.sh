82ce9480769d54932a0d7e0d2395a73d537a21ab
Jean-Francois Dockes <jfd@recoll.org>
2010-04-15 13:57:21 +0200

    none

---
+++ packaging/debian/buildppa.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+RCLVERSION=1.13.04
+
+debdir=debianrclqt3
+
+rm -rf recoll-${RCLVERSION}/debian
+cp -rp $debdir recoll-${RCLVERSION}/debian
+
+for series in dapper hardy interpid jaunty ;do 
+  sed -e s/SERIES/$series/g < ${debdir}/changelog > \
+    recoll-${RCLVERSION}/debian/changelog ;
+  (cd recoll-${RCLVERSION};debuild -S -sa) 
+  dput recoll-ppa recoll_${RCLVERSION}-0~ppa1~${series}1_source.changes
+done
+
+debdir=debianrclqt4
+
+rm -rf recoll-${RCLVERSION}/debian
+cp -rp $debdir recoll-${RCLVERSION}/debian
+
+for series in karmic ; do
+  sed -e s/SERIES/$series/g < ${debdir}/changelog > \
+    recoll-${RCLVERSION}/debian/changelog ;
+  (cd recoll-${RCLVERSION};debuild -S -sa) 
+  dput recoll-ppa recoll_${RCLVERSION}-0~ppa1~${series}1_source.changes
+done
+
