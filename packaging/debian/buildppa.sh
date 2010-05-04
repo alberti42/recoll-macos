10b486c600bd198a9c7cb8d25826913fc747fffc
Jean-Francois Dockes <jfd@recoll.org>
2010-05-04 15:36:23 +0200

    improve buildppa script. fix changelogs

--- packaging/debian/buildppa.sh
+++ packaging/debian/buildppa.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
-RCLVERSION=1.13.04
+RCLVERS=1.13.04
+PPAVERS=1
+
+########## QT3
+series3=""
+series3="dapper hardy intrepid"
 
 debdir=debianrclqt3
 
-rm -rf recoll-${RCLVERSION}/debian
-cp -rp $debdir recoll-${RCLVERSION}/debian
-
-for series in dapper hardy intrepid jaunty ;do 
+rm -rf recoll-${RCLVERS}/debian
+cp -rp $debdir recoll-${RCLVERS}/debian
+for series in  $series3;do 
   sed -e s/SERIES/$series/g < ${debdir}/changelog > \
-    recoll-${RCLVERSION}/debian/changelog ;
-  (cd recoll-${RCLVERSION};debuild -S -sa) 
-  dput recoll-ppa recoll_${RCLVERSION}-0~ppa1~${series}1_source.changes
+    recoll-${RCLVERS}/debian/changelog ;
+  (cd recoll-${RCLVERS};debuild -S -sa) 
+  dput recoll-ppa recoll_${RCLVERS}-0~ppa${PPAVERS}~${series}1_source.changes
 done
 
+####### QT4
+series4=""
+#series4="jaunty karmic lucid"
+series4="jaunty lucid"
+
 debdir=debianrclqt4
-
-rm -rf recoll-${RCLVERSION}/debian
-cp -rp $debdir recoll-${RCLVERSION}/debian
-
-for series in karmic ; do
+rm -rf recoll-${RCLVERS}/debian
+cp -rp $debdir recoll-${RCLVERS}/debian
+for series in $series4 ; do
   sed -e s/SERIES/$series/g < ${debdir}/changelog > \
-    recoll-${RCLVERSION}/debian/changelog ;
-  (cd recoll-${RCLVERSION};debuild -S -sa) 
-  dput recoll-ppa recoll_${RCLVERSION}-0~ppa1~${series}1_source.changes
+    recoll-${RCLVERS}/debian/changelog ;
+  (cd recoll-${RCLVERS};debuild -S -sa) 
+  dput recoll-ppa recoll_${RCLVERS}-0~ppa${PPAVERS}~${series}1_source.changes
 done
 
