25ff728317a0664a1c0bf910d4815f7b463cb09d
Jean-Francois Dockes <jfd@recoll.org>
2010-09-13 16:02:06 +0200

    indexedmimetypes test

---
+++ tests/indexedmimetypes/indexedmimetypes.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+RECOLL_CONFDIR=$topdir/$myname
+export RECOLL_CONFDIR
+logfilename=$toptmp/$myname.log
+
+sharedconf() {
+cat > recoll.conf <<EOF
+topdirs = $tstdata/html $tstdata/txt
+logfilename=$logfilename
+loglevel = 6
+
+EOF
+}
+###########
+# End boilerplate
+
+
+sharedconf
+cat >> recoll.conf <<EOF
+indexedmimetypes = text/plain
+indexallfilenames = 0
+EOF
+
+recollindex -z
+
+(
+# text files are indexed 
+recollq shorty
+# html ones are not (not even the names)
+recollq qtextedit
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+sharedconf
+cat >> recoll.conf <<EOF
+indexedmimetypes = text/plain
+indexallfilenames = 1
+EOF
+
+recollindex -z
+
+(
+# text files are indexed 
+recollq shorty
+# html ones are not (but the names are)
+recollq qtextedit
+recollq template
+) 2> $mystderr | egrep -v '^Recoll query: ' >> $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
