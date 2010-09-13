8a081503537669eb54e461b06ce1b028a7bcfe59
Jean-Francois Dockes <jfd@recoll.org>
2010-09-13 13:58:23 +0200

    date: searches tests

---
+++ tests/dates/dates.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+# 2nd one result
+
+(
+# no results because of the negative clause
+recollq date:/2009 -1 datetest
+# one result: the 2008 file
+recollq date:/2009    datetest
+# one result: 2008 file
+recollq date:2007-01-01/2010-02-09 datetest
+# 2 results : files from 10 and 11 march 2010
+recollq date:2010-03-10/P10D datetest
+# 0 result: no such term
+recollq date:2010-03-10/P10D datetestnotermsmatch
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
