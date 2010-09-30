13d550a40e57cf024bbfc0b9c37ce66bb8d73b60
Jean-Francois Dockes <jfd@recoll.org>
2010-09-30 15:23:52 +0200

    rcluncomp had trouble with multiple whitespace in file names because it used echo for output. Replaced by here document. Closes issue #27

---
+++ tests/compressed/compressed.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+(
+recollq ASPCSPCCONTENT
+recollq BSPCSPCCONTENT
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
