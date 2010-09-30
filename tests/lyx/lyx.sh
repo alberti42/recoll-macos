910f99553009e1fa7cd5a6edcb284994796f4ba1
Jean-Francois Dockes <jfd@recoll.org>
2010-09-30 17:49:38 +0200

    adapt rcllyx to newer lyx which apparently always export utf-8

--- tests/lyx/lyx.sh
+++ tests/lyx/lyx.sh
@@ -5,12 +5,11 @@ topdir=`dirname $0`/..
 
 initvariables $0
 
-recollq 'Bienvenue Dans Univers De Lyx' 2> $mystderr | 
-	egrep -v '^Recoll query: ' > $mystdout
-recollq 'Welcome To Lyx' 2>> $mystderr | 
-	egrep -v '^Recoll query: ' >> $mystdout
-recollq 'Udvozli Ont A LyX' 2>> $mystderr | 
-	egrep -v '^Recoll query: ' >> $mystdout
+(
+recollq 'Bienvenue Dans Univers De Lyx' 
+recollq 'Welcome To Lyx' 
+recollq 'Udvozli Ont A LyX' 
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
 
 diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
 
