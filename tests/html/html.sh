36a97cb8aa132e15d5a2a1e61ee7d7f199ba3543
Jean-Francois Dockes <jfd@recoll.org>
2011-06-24 11:08:12 +0200

    test: added html field extraction test

--- tests/html/html.sh
+++ tests/html/html.sh
@@ -8,7 +8,11 @@ initvariables $0
 recollq QMapConstIterator
 recollq Qtextedit Widget Provides Powerful Single-Page 
 recollq '"This is the Mysql reference manual"' 
+# Tests that the charset spec is correctly recognised inside badhtml.html
 recollq -a 'etonne badhtml' 
+# Tests field extraction/storage and indexing
+recollq -m -q "testfield:testfieldvalue" | egrep 'results|^text/html|^testfield ='
+
 ) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
 
 diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
