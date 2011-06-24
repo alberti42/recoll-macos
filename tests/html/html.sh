8fe524bd7f7e5b5dc6131f4df465fcb4df7117b7
Jean-Francois Dockes <jfd@recoll.org>
2011-06-24 10:40:29 +0200

    add html charset test

--- tests/html/html.sh
+++ tests/html/html.sh
@@ -4,13 +4,12 @@ topdir=`dirname $0`/..
 . $topdir/shared.sh
 
 initvariables $0
-
-recollq QMapConstIterator 2> $mystderr | 
-	egrep -v '^Recoll query: ' > $mystdout
-recollq Qtextedit Widget Provides Powerful Single-Page 2>> $mystderr | 
-	egrep -v '^Recoll query: ' >> $mystdout
-recollq '"This is the Mysql reference manual"' 2>> $mystderr | 
-	egrep -v '^Recoll query: ' >> $mystdout
+(
+recollq QMapConstIterator
+recollq Qtextedit Widget Provides Powerful Single-Page 
+recollq '"This is the Mysql reference manual"' 
+recollq -a 'etonne badhtml' 
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
 
 diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
 
