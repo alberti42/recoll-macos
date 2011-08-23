753636a6e6756ea874523d77706f22347b052397
"Jean-Francois Dockes ext:(%22) <jfd@recoll.org>
2011-08-23 10:59:20 +0200

    rar test

---
+++ tests/rar/rar.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+(
+  recollq -m -q sheetgen '"Are you sure you want to delete this sheet"' 
+
+)  2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
