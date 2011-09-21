8b239e432b73d4a219e875c2c84178188d6ba7c3
Jean-Francois Dockes <jfd@recoll.org>
2011-09-21 13:47:31 +0200

    test deeply embedded document

---
+++ tests/embed/embed.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+(
+for q in xing;do 
+    echo $q
+    recollq -q $q
+done
+
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
