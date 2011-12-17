502f7e783eaf2c309ba7f4410e9299f989e99a2b
Jean-Francois Dockes <jfd@recoll.org>
2011-12-17 16:41:45 +0100

    chm filter: handle files lacking a topics node

--- tests/chm/chm.sh
+++ tests/chm/chm.sh
@@ -5,14 +5,12 @@ topdir=`dirname $0`/..
 
 initvariables $0
 
-recollq '"nokia ovi suite" wmdrm "windows media player version 11"' \
-2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
-
-recollq '"pour superposer mixer des fichiers son"' \
-2>> $mystderr | egrep -v '^Recoll query: ' >> $mystdout
-
-recollq '"Django comes with a user authentication system"' \
-2>> $mystderr | egrep -v '^Recoll query: ' >> $mystdout
+(
+recollq '"nokia ovi suite" wmdrm "windows media player version 11"' 
+recollq '"pour superposer mixer des fichiers son"'
+recollq '"Django comes with a user authentication system"'
+recollq '"establishment of a project cost accounting system of ledgers"'
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
 
 diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
 
