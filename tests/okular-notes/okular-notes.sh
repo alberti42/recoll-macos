94989747ba54fbf931b1e06f0113a9e673883ad5
Jean-Francois Dockes <jfd@recoll.org>
2012-01-23 21:19:02 +0100

    test: okular notes

---
+++ tests/okular-notes/okular-notes.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+(
+    recollq  -q okularnote
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
