17542969a567834b01b33d6f798480b411f48dd3
Jean-Francois Dockes <jfd@recoll.org>
2012-01-23 20:25:55 +0100

    new gnumeric and okular notes filters

---
+++ tests/gnumeric/gnumeric.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+(
+    recollq author=gnumericAuthor 
+    recollq gnumerictext
+    recollq gnumericcommentaire
+) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
