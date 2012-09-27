4812965693c16e44ded04c06e5d582173725f7e4
"Jean-Francois Dockes ext:(%22) <jfd@recoll.org>
2012-09-27 20:12:24 +0200

    added a few case and diacritics sensitivity test cases

---
+++ tests/casediac/casediac.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# Case and diacritics sensitivity testing
+# We use a separate configuration for this.
+# This file is encoded in UTF-8: accentué
+
+LANG=en_US.UTF-8
+export LANG
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+RECOLL_CONFDIR=$topdir/casediac
+export RECOLL_CONFDIR
+
+cat > $RECOLL_CONFDIR/recoll.conf <<EOF
+loglevel = 6
+logfilename = /tmp/logrcltst
+
+daemloglevel = 6
+daemlogfilename = /tmp/rclmontrace
+
+indexStripChars = 0
+
+topdirs = $tstdata/casediac
+indexstemminglanguages = french
+
+EOF
+
+recollindex -z
+
+(
+set -x
+# Diac-sensitive search for existing term
+recollq -s french éviter
+# Diac-sensitive searches for non-existing terms
+recollq -s french èviter
+recollq -s french '"eviter"D'
+# Diac-unsensitive search
+recollq -s french eviter
+# Search for stem-existing term (has to be unsensitive)
+recollq -s french evite
+# Search for stem-existing term with disabled stemming
+recollq -s french Evite
+# Case-sensitive, existing and non existing
+recollq -s french MAJUSCULESXX
+recollq -s french MAjUSCULESXX
+# Case-unsensitive
+recollq -s french majusculesxx
+# Case-unsensitive
+recollq -s french Majusculesxx
+# Case sensitive, term does not exist
+recollq -s french '"Majusculesxx"C'
+
+)  2>&1 | egrep -v '^Recoll query: ' > $mystdout
+
+cd $RECOLL_CONFDIR 
+rm -rf history idxstatus.txt index.pid missing recoll.conf xapiandb/
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+checkresult
+
