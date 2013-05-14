5a5f9815673ceaaedbe26d668c2b4cd5a81e8dbc
Jean-Francois Dockes <jfd@recoll.org>
2013-05-14 10:21:13 +0200

    debian build fixes

--- packaging/debian/buildppa.sh
+++ packaging/debian/buildppa.sh
@@ -4,16 +4,16 @@
 # For the kio: (and kdesdk?)
 # sudo apt-get install pkg-kde-tools  cdbs
 
-RCLVERS=3304
-LENSVERS=1.18.2.3257
-PPAVERS=2
+RCLVERS=1.19.2
+LENSVERS=1.19.2.3328
+PPAVERS=3
 
 case $RCLVERS in
     [23]*) PPANAME=recollexp-ppa;;
     1.14*) PPANAME=recoll-ppa;;
     *)     PPANAME=recoll15-ppa;;
 esac
-#PPANAME=recollexp-ppa
+PPANAME=recollexp-ppa
 
 echo "PPA: $PPANAME. Type CR if Ok, else ^C"
 read rep
@@ -21,11 +21,11 @@ read rep
 ####### QT4
 debdir=debianrclqt4
 series4="lucid oneiric precise quantal raring"
-series4="precise"
+series4=
 
 for series in $series4 ; do
   rm -rf recoll-${RCLVERS}/debian
-  cp -rp ${debdir}/ recoll-${RCLVERS}/debian
+  cp -rp ${debdir}/ recoll-${RCLVERS}/debian || exit 1
 
   if test -f $debdir/control-$series ; then
       cp -f -p $debdir/control-$series recoll-${RCLVERS}/debian/control
@@ -44,20 +44,25 @@ done
 
 ### KIO
 seriesk="lucid oneiric precise quantal raring"
-seriesk=""
+seriesk=precise
 
 debdir=debiankio
+topdir=kio-recoll-${RCLVERS}
+test -d kio-recoll_${RCLVERS}.orig.tar.gz || \
+    cp -p recoll_${RCLVERS}.orig.tar.gz kio-recoll_${RCLVERS}.orig.tar.gz || \
+    exit 1
+test -d $topdir || cp -rp recoll-${RCLVERS} $topdir
 
 for series in $seriesk ; do
 
-  rm -rf recoll-${RCLVERS}/debian
-  cp -rp ${debdir}/ recoll-${RCLVERS}/debian
+  rm -rf $topdir/debian
+  cp -rp ${debdir}/ $topdir/debian || exit 1
 
   sed -e s/SERIES/$series/g \
       -e s/PPAVERS/${PPAVERS}/g \
-          < ${debdir}/changelog > recoll-${RCLVERS}/debian/changelog ;
+          < ${debdir}/changelog > $topdir/debian/changelog ;
 
-  (cd recoll-${RCLVERS};debuild -S -sa) || break
+  (cd $topdir;debuild -S -sa) || break
 
   dput $PPANAME kio-recoll_${RCLVERS}-0~ppa${PPAVERS}~${series}1_source.changes
 
@@ -65,20 +70,21 @@ done
 
 ### Unity Lens
 seriesl="oneiric precise quantal raring"
-seriesl=""
+seriesl=
 
 debdir=debianunitylens
+topdir=recoll-lens-${LENSVERS}
 
 for series in $seriesl ; do
 
-   rm -rf recoll-lens-${LENSVERS}/debian
-   cp -rp ${debdir}/ recoll-lens-${LENSVERS}/debian
+   rm -rf $topdir/debian
+   cp -rp ${debdir}/ $topdir/debian || exit 1
 
   sed -e s/SERIES/$series/g \
       -e s/PPAVERS/${PPAVERS}/g \
-          < ${debdir}/changelog > recoll-lens-${LENSVERS}/debian/changelog ;
+          < ${debdir}/changelog > $topdir/debian/changelog ;
 
-  (cd recoll-lens-${LENSVERS};debuild -S -sa) || break
+  (cd $topdir;debuild -S -sa) || break
 
   dput $PPANAME recoll-lens_${LENSVERS}-1~ppa${PPAVERS}~${series}1_source.changes
 
