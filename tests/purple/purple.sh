7837558909c40cf333c82dae86f79db85bbb2fbc
Jean-Francois Dockes <jfd@recoll.org>
2014-10-01 11:37:20 +0200

    rclpurple: fix for current log format

---
+++ tests/purple/purple.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+topdir=`dirname $0`/..
+. $topdir/shared.sh
+
+initvariables $0
+
+(
+recollq -S url '"Hallo friend"' date:2011-08-25
+recollq -S url '"I like the G+ post you gave a"' date:2014-09-22
+)  2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
+
+diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
+
+checkresult
