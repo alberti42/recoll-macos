bd58ffb920c53f4ee1bf8f438c85f52a1ab72908
Jean-Francois Dockes <jf@dockes.org>
2015-09-10 17:39:49 +0200

    open xml python + xslt filter
    --HG--
    branch : WINDOWSPORT

--- src/filters/rclsoff.py
+++ src/filters/rclsoff.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
+# Copyright (C) 2014 J.F.Dockes
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+######################################
 
 import sys
-import os
-import subprocess
 import rclexecm
 import rclxslt
 from zipfile import ZipFile
@@ -170,10 +184,6 @@ class OOExtractor:
             return ret
 
 if __name__ == '__main__':
-    # Check for unzip
-    if not rclexecm.which("unzip"):
-        print("RECFILTERROR HELPERNOTFOUND unzip")
-        sys.exit(1)
     proto = rclexecm.RclExecM()
     extract = OOExtractor(proto)
     rclexecm.main(proto, extract)
