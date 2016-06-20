fea94afcdd5f5bec11525f8b268f8e4285717948
Jean-Francois Dockes <jfd@recoll.org>
2016-06-20 16:35:49 +0200

    fix ref test results

--- tests/info/info.sh
+++ tests/info/info.sh
@@ -5,6 +5,14 @@ topdir=`dirname $0`/..
 
 initvariables $0
 
+# Note: file -i returns application/octet-stream for
+# somefile.info-1. xdg-mime query filetype returns text/plain. Recoll
+# 1.22 and later return several results for the following query, one
+# for the info file, and others in the somefile.info-xx files, as
+# text/plain files. Previous versions only returned the info file,
+# which was better, but I'm not sure that we can do something about
+# it.
+
 recollq '"GPGME is compiled with largefile support by default"' 2> $mystderr | 
 	egrep -v '^Recoll query: ' > $mystdout
 
