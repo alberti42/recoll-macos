116318f1f5b38b4007c1f83cdb7f0501ae725a2d
Jean-Francois Dockes <jf@dockes.org>
2018-02-09 09:30:41 +0100

    Added small script to process bibtex files

---
+++ src/filters/rclbibtex.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Remove unwanted data from bibtex file by deleting "bad" lines and
+# cleaning "good" ones. 
+
+if test "X$RECOLL_FILTER_FORPREVIEW" = "Xyes" ; then
+    sed \
+        -e '/^$/N;/^\n$/D' \
+        -e '/(bibdsk.*|month|bibsource|crossref|ee|groups|owner|pages|timestamp|url|file|price|citeulike.*|markedentry|posted-at)[[:space:]=]*/I d' \
+        -e '/@[^{]*{/ d' \
+        \
+        -e 's/{//g' \
+        -e 's/},\?$//g' \
+        -e 's/^[ 	]*//' \
+        < $1
+else
+    sed -e '/(bibdsk.*|month|bibsource|crossref|ee|groups|owner|pages|timestamp|url|file|price|citeulike.*|markedentry|posted-at)[[:space:]=]*/I d' \
+        -e '/@[^{]*{/ d' \
+        \
+        -e 's/^[^{]*{//g' \
+        -e 's/},\?$//g' \
+        -e 's/({|}|=)//g' \
+        < $1
+fi
