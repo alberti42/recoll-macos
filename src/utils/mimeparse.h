385a7b9547e07205e5e83ccd742f3fd4e9b36f36
Jean-Francois Dockes <jf@dockes.org>
2018-11-22 14:25:47 +0100

    indent + comments + explicit std::

--- src/utils/mimeparse.h
+++ src/utils/mimeparse.h
@@ -17,32 +17,32 @@
 #ifndef _MIME_H_INCLUDED_
 #define _MIME_H_INCLUDED_
 /*
-Mime definitions RFC to 4-9-2006:
+  Mime definitions RFC to 4-9-2006:
 
-2045 Multipurpose Internet Mail Extensions (MIME) Part One: Format of
-     Internet Message Bodies. N. Freed, N. Borenstein. November 1996.
-     (Format: TXT=72932 bytes) (Obsoletes RFC1521, RFC1522, RFC1590)
-     (Updated by RFC2184, RFC2231) (Status: DRAFT STANDARD)
+  2045 Multipurpose Internet Mail Extensions (MIME) Part One: Format of
+  Internet Message Bodies. N. Freed, N. Borenstein. November 1996.
+  (Format: TXT=72932 bytes) (Obsoletes RFC1521, RFC1522, RFC1590)
+  (Updated by RFC2184, RFC2231) (Status: DRAFT STANDARD)
 
-2046 Multipurpose Internet Mail Extensions (MIME) Part Two: Media
-     Types. N. Freed, N. Borenstein. November 1996. (Format: TXT=105854
-     bytes) (Obsoletes RFC1521, RFC1522, RFC1590) (Updated by RFC2646,
-     RFC3798) (Status: DRAFT STANDARD)
+  2046 Multipurpose Internet Mail Extensions (MIME) Part Two: Media
+  Types. N. Freed, N. Borenstein. November 1996. (Format: TXT=105854
+  bytes) (Obsoletes RFC1521, RFC1522, RFC1590) (Updated by RFC2646,
+  RFC3798) (Status: DRAFT STANDARD)
 
-2047 MIME (Multipurpose Internet Mail Extensions) Part Three: Message
-     Header Extensions for Non-ASCII Text. K. Moore. November 1996.
-     (Format: TXT=33262 bytes) (Obsoletes RFC1521, RFC1522, RFC1590)
-     (Updated by RFC2184, RFC2231) (Status: DRAFT STANDARD)
+  2047 MIME (Multipurpose Internet Mail Extensions) Part Three: Message
+  Header Extensions for Non-ASCII Text. K. Moore. November 1996.
+  (Format: TXT=33262 bytes) (Obsoletes RFC1521, RFC1522, RFC1590)
+  (Updated by RFC2184, RFC2231) (Status: DRAFT STANDARD)
 
-2183 Communicating Presentation Information in Internet Messages: The
-     Content-Disposition Header Field. R. Troost, S. Dorner, K. Moore,
-     Ed.. August 1997. (Format: TXT=23150 bytes) (Updates RFC1806)
-     (Updated by RFC2184, RFC2231) (Status: PROPOSED STANDARD)
+  2183 Communicating Presentation Information in Internet Messages: The
+  Content-Disposition Header Field. R. Troost, S. Dorner, K. Moore,
+  Ed.. August 1997. (Format: TXT=23150 bytes) (Updates RFC1806)
+  (Updated by RFC2184, RFC2231) (Status: PROPOSED STANDARD)
 
-2231 MIME Parameter Value and Encoded Word Extensions: Character Sets,
-     Languages, and Continuations. N. Freed, K. Moore. November 1997.
-     (Format: TXT=19280 bytes) (Obsoletes RFC2184) (Updates RFC2045,
-     RFC2047, RFC2183) (Status: PROPOSED STANDARD)
+  2231 MIME Parameter Value and Encoded Word Extensions: Character Sets,
+  Languages, and Continuations. N. Freed, K. Moore. November 1997.
+  (Format: TXT=19280 bytes) (Obsoletes RFC2184) (Updates RFC2045,
+  RFC2047, RFC2183) (Status: PROPOSED STANDARD)
 */
 
 
@@ -53,15 +53,11 @@ Mime definitions RFC to 4-9-2006:
 
 #include "base64.h"
 
-#ifndef NO_NAMESPACES
-using std::string;
-#endif
-
 /** A class to represent a MIME header value with parameters */
 class MimeHeaderValue {
- public:
-    string value;
-    std::map<string, string> params;
+public:
+    std::string value;
+    std::map<std::string, std::string> params;
 };
 
 /** 
@@ -70,14 +66,17 @@ class MimeHeaderValue {
  * @param in the input string should be like: value; pn1=pv1; pn2=pv2. 
  *   Example: text/plain; charset="iso-8859-1" 
  */
-extern bool parseMimeHeaderValue(const string& in, MimeHeaderValue& psd);
+extern bool parseMimeHeaderValue(const std::string& in, MimeHeaderValue& psd);
 
 /** 
- * Quoted printable decoding. Doubles up as rfc2231 decoder, hence the esc 
- * RFC2045 Quoted printable uses '=' , rfc2331 uses '%'. The two encodings are
+ * Quoted Printable decoding. 
+ *
+ * Doubles up as rfc2231 decoder, with the help of the hence the @param esc 
+ * parameter.
+ * RFC2045 Quoted Printable uses '=' , RFC2331 uses '%'. The two encodings are
  * otherwise similar.
  */
-extern bool qp_decode(const string& in, string &out, char esc = '=');
+extern bool qp_decode(const std::string& in, std::string &out, char esc = '=');
 
 /** Decode an Internet mail field value encoded according to rfc2047 
  *
@@ -90,14 +89,14 @@ extern bool qp_decode(const string& in, string &out, char esc = '=');
  * @param in input string, ascii with rfc2047 markup
  * @return out output string encoded in utf-8
  */
-extern bool rfc2047_decode(const string& in, string &out);
+extern bool rfc2047_decode(const std::string& in, std::string &out);
 
 
-/** Decode RFC2822 date to unix time (gmt secs from 1970
+/** Decode RFC2822 date to unix time (gmt secs from 1970)
  *
  * @param dt date string (the part after Date: )
  * @return unix time
  */
-time_t rfc2822DateToUxTime(const string& dt);
+time_t rfc2822DateToUxTime(const std::string& dt);
 
 #endif /* _MIME_H_INCLUDED_ */
