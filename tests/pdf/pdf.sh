4c205e44e0c5dcf130e61077694eddbc64ec1279
Jean-Francois Dockes <jf@dockes.org>
2019-06-12 19:22:30 +0200

    tests: test the xmp metadata extraction

--- tests/pdf/pdf.sh
+++ tests/pdf/pdf.sh
@@ -5,8 +5,14 @@ topdir=`dirname $0`/..
 
 initvariables $0
 
-recollq '"sequences of moveto and lineto"' OR 'ANSIX3.4' 2> $mystderr | 
-	egrep -v '^Recoll query: ' > $mystdout
+(
+    recollq '"sequences of moveto and lineto"' OR 'ANSIX3.4'
+    # pdf xmp. Note that dc:identifier is aliased to url in the
+    # defaults field file, and this can't be overruled afaics, so
+    # url is prefixed for the dc:identifier search to work
+    recollq dc:identifier:10.12345/sampledoi
+    recollq 'pdf:Producer:"GPL Ghostscript 9.18"'
+)  2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
 
 diff -w ${myname}.txt $mystdout > $mydiffs 2>&1
 
