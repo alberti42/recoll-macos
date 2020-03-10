2cbd9ad79cd0e79e8bac9bf68752d121ed126d0f
Jean-Francois Dockes <jf@dockes.org>
2020-03-10 14:38:52 +0100

    Added handler for Hancom .hwp format

--- src/filters/rclpdf.py
+++ src/filters/rclpdf.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2014 J.F.Dockes
+# Copyright (C) 2014-2020 J.F.Dockes
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
