d85bf0ba41fba3d573282bcae42101043f23fbbf
Jean-Francois Dockes <jf@dockes.org>
2020-04-06 15:23:58 +0100

    Deps on win10 msvc ok except aspell

--- src/windows/mkinstdir.sh
+++ src/windows/mkinstdir.sh
@@ -29,24 +29,24 @@ if test $BUILD = MSVC ; then
     # Recoll src tree
     RCL=c:/users/bill/documents/recoll/src/
     # Recoll dependancies
-    RCLDEPS=c:/users/bill/documents/recolldeps-vc/
+    RCLDEPS=c:/users/bill/documents/recolldeps/
     QTA=Desktop_Qt_5_14_1_MSVC2017_32bit-Release/release
-    LIBXAPIAN=${RCL}windows/build-libxapian-${QTA}/libxapian.dll
-    LIBXML=${RCLDEPS}libxml2/libxml2-2.9.4+dfsg1/win32/bin.msvc/libxml2.dll
-    LIBXSLT=${RCLDEPS}libxslt/libxslt-1.1.29/win32/bin.msvc/libxslt.dll
-    LIBICONV=${RCLDEPS}build-libiconv-Desktop_Qt_5_14_1_MSVC2017_32bit-Release/release/libiconv.dll
-    ZLIB=${RCLDEPS}zlib-1.2.11
+    LIBXML=${RCLDEPS}/msvc/libxml2/libxml2-2.9.4+dfsg1/win32/bin.msvc/libxml2.dll
+    LIBXSLT=${RCLDEPS}/msvc/libxslt/libxslt-1.1.29/win32/bin.msvc/libxslt.dll
+    LIBICONV=${RCLDEPS}/msvc/build-libiconv-${QTA}/libiconv.dll
+    ZLIB=${RCLDEPS}/msvc/zlib-1.2.11
     QTBIN=C:/Qt/5.14.1/msvc2017/bin
 else
     # Recoll src tree
     RCL=c:/recoll/src/
     # Recoll dependancies
     RCLDEPS=c:/recolldeps/
-    LIBXAPIAN=${RCLDEPS}xapian-core-1.4.11/.libs/libxapian-30.dll
-    ZLIB=${RCLDEPS}zlib-1.2.8
-    QTBIN=C:/Qt/Qt5.8.0/5.8/mingw53_32/bin
-    QTGCCBIN=C:/qt/Qt5.8.0/Tools/mingw530_32/bin/
     QTA=Desktop_Qt_5_8_0_MinGW_32bit/release
+    LIBXAPIAN=${RCLDEPS}/mingw/xapian-core-1.4.11/.libs/libxapian-30.dll
+    ZLIB=${RCLDEPS}/mingw/zlib-1.2.8
+    ASPELL=${RCLDEPS}/mingw/aspell-0.60.7/aspell-installed
+    QTGCCBIN=C:/qt/Qt5.8.0/Tools/mingw530_32/bin/
+    QTBIN=C:/Qt/Qt5.8.0/5.8/mingw53_32/bin
     PATH=$MINGWBIN:$QTGCCBIN:$PATH
     export PATH
 fi
@@ -55,8 +55,10 @@ fi
 # c:/MinGW (as opposed to the mingw bundled with qt). This is the same
 # for either a msvc or mingw build of recoll itself.
 MINGWBIN=C:/MinGW/bin
+MINGWBIN=c:/Qt/5.14.1/mingw73_32/bin/
 
 RCLW=$RCL/windows/
+# Only used for mingw, the msvc one is static
 LIBR=$RCLW/build-librecoll-${QTA}/${qtsdir}/librecoll.dll
 GUIBIN=$RCL/build-recoll-win-${QTA}/${qtsdir}/recoll.exe
 RCLIDX=$RCLW/build-recollindex-${QTA}/${qtsdir}/recollindex.exe
@@ -77,7 +79,6 @@ LIBREVENGE=${RCLDEPS}libwpd/librevenge-0.0.1.jfd/
 CHM=${RCLDEPS}pychm
 MISC=${RCLDEPS}misc
 LIBPFF=${RCLDEPS}pffinstall
-ASPELL=${RCLDEPS}/aspell-0.60.7/aspell-installed
 
 ################
 # Script:
@@ -104,6 +105,7 @@ copyqt()
     PATH=$QTBIN:$PATH
     export PATH
     $QTBIN/windeployqt recoll.exe
+
     if test $BUILD = MINGW;then
         # Apparently because the webkit part was grafted "by hand" on
 	# the Qt set, we need to copy some dll explicitly
@@ -128,6 +130,7 @@ copypython()
     cp -rp $PYTHON/* $DESTDIR/Share/filters/python
     chkcp $PYTHON/python.exe $DESTDIR/Share/filters/python/python.exe
 }
+
 copyrecoll()
 {
     
@@ -136,14 +139,13 @@ copyrecoll()
     chkcp $RCLQ $DESTDIR 
     chkcp $RCLS $DESTDIR 
     chkcp $ZLIB/zlib1.dll $DESTDIR
-    chkcp $LIBXAPIAN $DESTDIR
     if test $BUILD = MINGW;then
+        chkcp $LIBXAPIAN $DESTDIR
 	chkcp $LIBR $DESTDIR 
         chkcp $MINGWBIN/libgcc_s_dw2-1.dll $DESTDIR
     else
 	chkcp $LIBXML $DESTDIR
 	chkcp $LIBXSLT $DESTDIR
-#	chkcp $LIBICONV $DESTDIR
     fi
     chkcp $RCL/COPYING                  $DESTDIR/COPYING.txt
     chkcp $RCL/doc/user/usermanual.html $DESTDIR/Share/doc
@@ -163,18 +165,15 @@ copyrecoll()
     rm -f $FILTERS/rclimg*
     chkcp $RCL/filters/*       $FILTERS
     rm -f $FILTERS/rclimg $FILTERS/rclimg.py
-#LATER    chkcp $RCLDEPS/rclimg/rclimg.exe $FILTERS
+    chkcp $RCLDEPS/rclimg/rclimg.exe $FILTERS
     chkcp $RCL/qtgui/mtpics/*  $DESTDIR/Share/images
     chkcp $RCL/qtgui/i18n/*.qm $DESTDIR/Share/translations
 }
 
 copyantiword()
 {
-    # MS VS
-    #bindir=$ANTIWORD/Win32-only/$PLATFORM/$CONFIGURATION
     # MINGW
     bindir=$ANTIWORD/
-
     test -d $Filters/Resources || mkdir -p $FILTERS/Resources || exit 1
     chkcp  $bindir/antiword.exe            $FILTERS
     chkcp  $ANTIWORD/Resources/*           $FILTERS/Resources
@@ -182,7 +181,6 @@ copyantiword()
 
 copyunrtf()
 {
-#    bindir=$UNRTF/Windows/$PLATFORM/$CONFIGURATION
      bindir=$UNRTF/Windows/
 
     test -d $FILTERS/Share || mkdir -p $FILTERS/Share || exit 1
@@ -262,7 +260,7 @@ copywpd()
     chkcp $MINGWBIN/libgcc_s_dw2-1.dll $DEST
     chkcp $MINGWBIN/libstdc++-6.dll $DEST
     chkcp $ZLIB/zlib1.dll $DEST
-    chkcp $QTBIN/libwinpthread-1.dll $DEST
+    chkcp $MINGWBIN/libwinpthread-1.dll $DEST
 }
 
 copychm()
@@ -279,18 +277,20 @@ copypff()
     chkcp $LIBPFF/mingw32/bin/pffexport.exe $DEST
     chkcp $MINGWBIN/libgcc_s_dw2-1.dll $DEST
     chkcp $MINGWBIN/libstdc++-6.dll $DEST
-    chkcp $QTBIN/libwinpthread-1.dll $DEST
+    chkcp $MINGWBIN/libwinpthread-1.dll $DEST
 }
 
 copyaspell()
 {
     DEST=$FILTERS
-    cp -rp $ASPELL $DEST || fatal "can't copy $ASPELL"
-    DEST=$DEST/aspell-installed/mingw32/bin
-    # Check that we do have an aspell.exe.
-    chkcp $ASPELL/mingw32/bin/aspell.exe $DEST
-    chkcp $MINGWBIN/libgcc_s_dw2-1.dll $DEST
-    chkcp $MINGWBIN/libstdc++-6.dll $DEST
+    if test $BUILD = MINGW ; then
+        cp -rp $ASPELL $DEST || fatal "can't copy $ASPELL"
+        DEST=$DEST/aspell-installed/mingw32/bin
+        # Check that we do have an aspell.exe.
+        chkcp $ASPELL/mingw32/bin/aspell.exe $DEST
+        chkcp $MINGWBIN/libgcc_s_dw2-1.dll $DEST
+        chkcp $MINGWBIN/libstdc++-6.dll $DEST
+    fi
 }
 
 # First check that the config is ok
@@ -310,25 +310,25 @@ for d in doc examples filters images translations; do
         fatal mkdir $d failed
 done
 
-#LATER copyaspell
+copyaspell
 # copyrecoll must stay before copyqt so that windeployqt can do its thing
 copyrecoll
 copyqt
-#LATER copypyxslt
-#LATER copypoppler
-#LATER copyantiword
-#LATER copyunrtf
+copypyxslt
+copypoppler
+copyantiword
+copyunrtf
 # Copied into python3 and installed with it
 #copyfuture
-#LATER copymutagen
+copymutagen
 # Switched to perl for lack of python3 version
 #copypyexiv2
-#LATER copywpd
+copywpd
 # Chm is now copied into the python tree, which is installed by copypython
 #copychm
-#LATER copypff
+copypff
 
 # Note that pychm and pyhwp are pre-copied into the py-python3 python
 # distribution directory. The latter also needs olefile and six (also
 # copied)
-#LATER copypython
+copypython
