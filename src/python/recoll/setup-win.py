9766fd983e5e5dfac4322f58a23f6c5f0ff35d8f
Jean-Francois Dockes <jf@dockes.org>
2020-04-15 14:47:44 +0100

    windows recoll python module setup.py

---
+++ src/python/recoll/setup-win.py
@@ -0,0 +1,72 @@
+from setuptools import setup, Extension
+import os
+import sys
+
+srcdir = "c:/users/bill/documents/recoll/src/python/recoll"
+recolldeps = "c:/users/bill/documents/recolldeps/msvc"
+
+# The compiled-in values are not used under Windows
+prefix = "c:/install/recoll"
+RECOLL_DATADIR = "c:/install/recoll/Share/"
+
+# For shadow builds: references to the source tree
+top = os.path.join(srcdir, '..', '..')
+pytop = srcdir
+localtop = top
+extra_compile_args = ''
+
+VERSION = open(os.path.join(top, "VERSION")).read().strip()
+
+include_dirs = [
+        os.path.join(top, 'common'),
+        os.path.join(top, 'rcldb'), 
+        os.path.join(top, 'xaposix'), 
+        os.path.join(top, 'utils'), 
+        os.path.join(top, 'query'), 
+        os.path.join(top, 'internfile'), 
+]
+define_macros = [
+        ('RECOLL_DATADIR', RECOLL_DATADIR),
+        ('__WIN32__', '1')
+]
+library_dirs = [
+        os.path.join(top, "windows", "build-librecoll-Desktop_Qt_5_14_1_MSVC2017_32bit-Release/release"),
+        os.path.join(recolldeps, "libxml2/libxml2-2.9.4+dfsg1/win32/bin.msvc"),
+        os.path.join(recolldeps, "libxslt/libxslt-1.1.29/win32/bin.msvc"),
+        os.path.join(top, "windows", "build-libxapian-Desktop_Qt_5_14_1_MSVC2017_32bit-Release/release"),
+        os.path.join(recolldeps, "zlib-1.2.11"),
+        os.path.join(recolldeps, "build-libiconv-Desktop_Qt_5_14_1_MSVC2017_32bit-Release/release")
+        ]
+libraries =  ["librecoll", "libxml2_a", "libxslt_a",
+              "libxapian", "libiconv", "zlib",
+              "rpcrt4", "ws2_32", "shlwapi", "shell32",
+              "psapi", "user32", "kernel32"
+]
+module1 = Extension('recoll',
+                    define_macros = define_macros,
+                    include_dirs = include_dirs,
+                    libraries = libraries,
+                    library_dirs = library_dirs,
+                    sources = [os.path.join(pytop, 'pyrecoll.cpp')])
+
+module2 = Extension('rclextract',
+                    define_macros = define_macros,
+                    include_dirs = include_dirs,
+                    extra_compile_args = extra_compile_args,
+                    libraries = libraries,
+                    library_dirs = library_dirs,
+                    sources = [os.path.join(pytop, 'pyrclextract.cpp')])
+
+setup (name = 'Recoll',
+       version = VERSION,
+       description = 'Query/Augment a Recoll full text index',
+       author = 'J.F. Dockes',
+       author_email = 'jfd@recoll.org',
+       url = 'http://www.recoll.org',
+       license = 'GPL',
+#       package_dir = {'' : os.path.join(top, 'python', 'recoll')},
+       long_description = '''
+''',
+    packages = ['recoll'],
+    ext_package = 'recoll',
+       ext_modules = [module1, module2])
