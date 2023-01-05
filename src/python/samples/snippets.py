fab5a71af6936bc7f4c0dec5a1a09949077edea9
Jean-Francois Dockes <jf@dockes.org>
2023-01-05 10:09:16 +0100

    improved snippets.py python sample

--- src/python/samples/snippets.py
+++ src/python/samples/snippets.py
@@ -1,10 +1,69 @@
 #!/usr/bin/python3
+# Copyright (C) 2022-2023 J.F.Dockes
+#
+# License: GPL 2.1
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import sys
+
+from getopt import getopt
 
 from recoll import recoll
 
-db  = recoll.connect()
-q = db.query()
-q.execute("orographic")
+def msg(s):
+    print(f"{s}", file=sys.stderr)
+    
+def Usage():
+    msg("Usage: snippets.py [-c conf] [-i extra_index] [-w ctxwords] <recoll query>")
+    sys.exit(1);
+
+if len(sys.argv) < 2:
+    Usage()
+
+
+confdir=""
+extra_dbs = []
+ctxwords = 4
+# Process options: [-c confdir] [-i extra_db [-i extra_db] ...]
+try:
+    options, args = getopt(sys.argv[1:], "c:i:w:")
+except Exception as ex:
+    print(f"{ex}")
+    sys.exit(1)
+for opt,val in options:
+    if opt == "-c":
+        confdir = val
+    elif opt == "-i":
+        extra_dbs.append(val)
+    elif opt == "-w":
+        ctxwords = int(val)
+    else:
+        print(f"Bad opt: {opt}")
+        Usage()
+
+if len(args) == 0:
+    msg("No query found in command line")
+    Usage()
+qs = " ".join(args)
+#msg(f"QUERY: [{qs}]")
+
+db = recoll.connect(confdir=confdir, extra_dbs=extra_dbs)
+query = db.query()
+query.execute(qs)
 
 class HL:
     def startMatch(self, i):
@@ -14,12 +73,10 @@ class HL:
 
 hlmeths = HL()
 
-for doc in q:
+for doc in query:
     print("DOC %s" % doc.title)
-    snippets = q.getsnippets(doc, maxoccs=-1, ctxwords=10, methods=hlmeths, sortbypage=False)
+    snippets = query.getsnippets(doc,
+                                 maxoccs=-1, ctxwords=ctxwords, methods=hlmeths, sortbypage=False)
     print("Got %d snippets" % len(snippets))
     for snip in snippets:
-        try:
-            print("Page %d term [%s] snippet [%s]" % (snip[0], snip[1], snip[2]))
-        except Exception as ex:
-            print("Print failed: %s" % ex)
+        print("Page %d term [%s] snippet [%s]" % (snip[0], snip[1], snip[2]))
