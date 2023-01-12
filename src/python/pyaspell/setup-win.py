2370a44da27cbbc0a1a54e530ddb6ff5c627b67e
Jean-Francois Dockes <jf@dockes.org>
2023-01-12 13:47:58 +0000

    pyaspell win setup

---
+++ src/python/pyaspell/setup-win.py
@@ -0,0 +1,41 @@
+from setuptools import setup, Extension
+
+# For shadow builds: references to the source tree
+import os
+root = "c:/users/bill/documents/"
+top = os.path.join(root, "recoll", "src")
+pytop = os.path.join(top, "python", "pyaspell")
+recolldeps = os.path.join(root, "recolldeps")
+
+setup (name = 'aspell-python-py3',
+    version = '1.15',
+    ext_modules = [Extension('aspell',
+                             [os.path.join(pytop, "aspell.c")],
+                             include_dirs = [
+                                 os.path.join(recolldeps,
+                                              "mingw", "aspell-0.60.7", "interfaces", "cc"), 
+                             ],
+                             libraries = ["aspell"],
+                             library_dirs = [
+                                 os.path.join(
+                                     recolldeps, "mingw",
+                                     "build-libaspell-Desktop_Qt_5_14_2_MSVC2017_32bit-Release",
+                                     "release")],
+                             )],
+    description      = "Wrapper around GNU Aspell for Python 3",
+    author           = "Wojciech Muła",
+    author_email     = "wojciech_mula@poczta.onet.pl",
+    maintainer       = "Wojciech Muła",
+    maintainer_email = "wojciech_mula@poczta.onet.pl",
+    url              = "http://github.com/WojciechMula/aspell-python",
+    platforms        = ["Linux", "Windows"],
+    license          = "BSD (3 clauses)",
+    long_description = "aspell-python - C extension for Python 3",
+    classifiers      = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: C",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Editors :: Text Processing",
+    ],
+)
