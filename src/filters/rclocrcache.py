ce5b15382187852d768a57c06f4c7a7d8dddb32f
Jean-Francois Dockes <jf@dockes.org>
2023-05-18 14:45:13 +0200

    doc

--- src/filters/rclocrcache.py
+++ src/filters/rclocrcache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-#################################
-# Copyright (C) 2020 J.F.Dockes
+##########################################################################
+# Copyright (C) 2020-2023 J.F.Dockes
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 2 of the License, or
@@ -15,70 +15,124 @@
 #   along with this program; if not, write to the
 #   Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-########################################################
+###########################################################################
+"""Caching the results of expensive processing of file documents
 
-# Caching OCR'd data
-#
-# OCR is extremely slow, caching the results is necessary.
-#
-# The cache stores 2 kinds of objects:
-# - Path files are named from the hash of the image file path and contain the
-#   image data hash, the modification time and size of the image file at the
-#   time the OCR'd data was stored in the cache, and the image path itself (the
-#   last is for purging only).
-# - Data files are named with the hash of the image data and contain the
-#   zlib-compressed OCR'd data.
-# - The cache Path and Data files are stored under top subdirectories: objects/
-#   and paths/.
-#
-# When retrieving data from the cache:
-#  - We first use the image file size and modification time: if an entry exists
-#    for the imagepath/mtime/size triplet, and is up to date, the corresponding
-#    data is obtained from the data file and returned.
-#  - Else we then use the image data: if an entry exists for the computed hashed
-#    value of the data, it is returned. This allows moving files around without
-#    needing to run OCR again, but of course, it is more expensive than the
-#    first step
-#
-# In both cases, the paths are hashed with sha1, and the first two characters of
-# the hash are used as a top level directory, the rest as a file name. E.g. for:
-#   pd,pf = self._hashpath(path), the result would be stored under pd/pf
-#
-# If we need to use the second step, as a side effect, a path file is created or
-# updated so that the data will be found with the first step next time around.
-#
-# When processing embedded documents like email attachments, recoll uses
-# temporary copies in TMPDIR (which defaults to /tmp) or RECOLL_TMPDIR. Of
-# course the paths for the temporary files changes when re-processing a given
-# document. We do not store the Path file for data stored in TMPDIR or
-# RECOLL_TMPDIR, because doing so would cause an indefinite accumulation of
-# unusable Path files. This means that access to the OCR data for these
-# documents always causes the computation of the data hash, and is slower. With
-# recent Recoll versions which cache the text content in the index, this only
-# occurs when reindexing (with older versions, this could also occur for
-# Preview).
-#
-# Purging the cache of obsolete data:
-#
-# This can be done by running this file as a top level script with a --purge
-# option (possibly completed by a --purgedata option but see below)
-#  - Purging the paths tree just involves walking it, reading the files, and
-#    checking the existence of the recorded paths. Path files for non-existent
-#    files are deleted.
-#  - Purging the data tree: we make a list of all Data files referenced by at
-#    least one Path file, then walk the data tree, deleting unreferenced
-#    files. This means that Data files from temporary document copies (see
-#    above) will be deleted, which is quite unsatisfying. This would be
-#    difficult to change:
-#    - There is no way to detect the affected files because the Data files store
-#      no origin information
-#    - Even if we wanted to store an indication that the data file comes from a
-#      temporary document, we'd have no way to access the original document
-#      because the full ipath is not available. Changing this would be close to
-#      impossible because internfile...
-# In consequence the --purgedata option must be explicitly added for a data
-# purge to be performed. Only set it if re-OCRing all embedded documents is reasonable.
+Overview:
+=========
 
+This module was initially created for caching the result of running OCR on image
+files, hence its name, but it was then reused for the output of speech
+recognition (STT, Speech To Text) on audio files. More generally, it could be
+used for caching the results of any expensive transformation performed on a
+document file.
+
+The storage is organised so that retrieving the cached data for an unchanged
+file is very fast, and retrieving it for a file which has been moved while
+retaining the same contents is much faster than the OCR or STT treatment, only
+needing a, possibly partial, contents hash computation.
+
+The cache is made of two separate directory trees: the paths tree holds files
+named by a hash of the document file path, and contain its path, size, recorded
+modification time and data hash. The data tree holds files named by a hash of
+the document file data, and containing the results of the OCR or STT operation.
+
+Interface:
+==========
+
+API:
+----
+
+The main programming interface is provided by the methods in the OCRCache class:
+store(), erase(), get(), along with some auxiliary methods for purging stale data.
+
+Script interface:
+-----------------
+
+This file can be run as a script with for performing operations on the cache,
+with different options. In all cases the cache affected is the one designated by
+the Recoll configuration determined by the RECOLL_CONFDIR environment variable
+(the -c option is not supported at the moment).
+
+rclocrcache.py --purge [--purgedata]
+++++++++++++++++++++++++++++++++++++
+
+Purge the cache of obsolete data.
+
+* --purge: purge the paths tree.  Walk the tree, read each path file, and
+  check the existence of the recorded path. If the recorded path does not exist
+  any more, delete the path file.
+* --purgedata: purge the data tree. Make a list of all data files referenced by
+  at least one path file, then walk the data tree, deleting unreferenced
+  files. This means that data files from temporary document copies (see further
+  below) will be deleted, which is quite unsatisfying. This would be difficult
+  to change:
+  - There is no way to detect the affected files because the data files store
+    no origin information.
+  - Even if we wanted to store an indication that the data file comes from a
+    temporary document, we'd have no way to access the original document
+    because the full ipath is not available. Changing this would be close to
+    impossible for Recoll internals reasons (internfile...).
+
+In consequence the --purgedata option must be explicitly added for a data purge
+to be performed. Only set it if re-OCRing all embedded documents is reasonable.
+
+rclocrcache.py --store <imgdatapath> <ocrdatapath>
+++++++++++++++++++++++++++++++++++++++++++++++++++
+Store data: create a cache entry for for the source file <imgdatapath>, and the
+processed data <ocrdatapath>.
+
+rclocrcache.py --get <imgdatapath>
+++++++++++++++++++++++++++++++++++
+Retrieve data stored for <imgdatapath>. The data is written to stdout.
+
+rclocrcache.py --erase <imgdatapath>
+++++++++++++++++++++++++++++++++++++
+Erase the cache contents for <imgdatapath>.
+
+
+Cache organisation details:
+============================
+
+The cache stores 2 kinds of objects:
+
+- Path files are named from the hash of the document file path and contain the
+  document data hash, the modification time and size of the document file at the
+  time it was processed, and the document file path itself (the last is for
+  purging only).
+- Data files are named with the hash of the document data and contain the
+  processed data (z-lib compressed).
+- The cache path and data files are stored under top subdirectories: objects/
+  and paths/
+- In both cases, the the first two characters of the path or data sha1 hash are
+  used as a subdirectory name, the rest as a file name, so a typical path would
+  look like *objects/xx/xxxxxxxxxx* or *paths/xx/xxxxxxxxxxx*.
+
+When retrieving data from the cache:
+
+- We first use the document file path, size, and modification time: if an entry
+  exists for the imagepath/mtime/size triplet, and is up to date, the
+  data file is obtained from the hash contained in the path file, and its
+  contents are returned.
+- Else we then use the document data: if an entry exists for the computed hashed
+  value of the data, it is returned. This allows moving files around without
+  needing to process them again, but of course, it is more expensive than the
+  first step.
+
+If we need to use the second step, as a side effect, a path file is created or
+updated so that the data will be found with the first step next time around.
+
+When processing embedded documents like email attachments, recoll uses
+temporary copies in TMPDIR (which defaults to /tmp) or RECOLL_TMPDIR. Of
+course the paths for the temporary files changes when re-processing a given
+document. We do not store the Path file for data stored in TMPDIR or
+RECOLL_TMPDIR, because doing so would cause an indefinite accumulation of
+unusable Path files. This means that access to the OCR data for these
+documents always causes the computation of the data hash, and is slower. With
+recent Recoll versions which cache the text content in the index, this only
+occurs when reindexing (with older versions, this could also occur for
+Preview).
+"""
 
 import sys
 import os
@@ -106,7 +160,8 @@ class OCRCache(object):
         """Initialize the cache object using parameters from conf, a RclConfig object.
         
         Keyword arguments:
-        hashmb -- if non zero we only hash the amount of data specified by the value (mb)
+        hashmb -- if non zero we only hash the amount of data specified by the value (mb). 
+          Data slices will be taken from the start, middle, and end of the file.
         """
         self.config = conf
         self.hashmb = hashmb
@@ -162,8 +217,8 @@ class OCRCache(object):
         return h[0:2], h[2:]
 
     def _readpathfile(self, ppf):
-        '''Read path file and return values. We do not decode the image path
-        as this is only used for purging'''
+        """Read path file and return values. We do not decode the image path
+        as this is only used for purging"""
         with open(ppf, 'r') as f:
             line = f.read()
         dd, df, tm, sz, pth = line.split()
@@ -247,7 +302,7 @@ class OCRCache(object):
         return True
 
     def erase(self, path):
-        '''Unconditionally erase cached data for input data file path'''
+        """Unconditionally erase cached data for input data file path"""
         ok=True
         pd, pf = self._hashpath(path)
         pfn = os.path.join(self.pathdir, pd, pf)
@@ -295,7 +350,7 @@ class OCRCache(object):
             return True, data
 
     def _pathstale(self, origpath, otm, osz):
-        '''Return True if the input path has been removed or modified'''
+        """Return True if the input path has been removed or modified"""
         if not os.path.exists(origpath):
             return True
         ntm = int(os.path.getmtime(origpath))
@@ -306,9 +361,9 @@ class OCRCache(object):
         return False
 
     def purgepaths(self):
-        '''Remove all stale pathfiles: source image does not exist or has
+        """Remove all stale pathfiles: source image does not exist or has
         been changed. Mostly useful for removed files, modified ones would be
-        processed by recollindex.'''
+        processed by recollindex."""
         allpathfiles = glob.glob(os.path.join(self.pathdir, "*", "*"))
         for pathfile in allpathfiles:
             dd, df, tm, sz, orgpath = self._readpathfile(pathfile)
@@ -318,8 +373,8 @@ class OCRCache(object):
                 os.remove(pathfile)
 
     def _walk(self, topdir, cb):
-        '''Specific fs walk: we know that our tree has 2 levels. Call cb with
-        the file path as parameter for each file'''
+        """Specific fs walk: we know that our tree has 2 levels. Call cb with
+        the file path as parameter for each file"""
         dlist = glob.glob(os.path.join(topdir, "*"))
         for dir in dlist:
             files = glob.glob(os.path.join(dir, "*"))
@@ -327,15 +382,15 @@ class OCRCache(object):
                 cb(f)
 
     def _pgdt_pathcb(self, f):
-        '''Get a pathfile name, read it, and record datafile identifier
-        (concatenate data file subdir and file name)'''
+        """Get a pathfile name, read it, and record datafile identifier
+        (concatenate data file subdir and file name)"""
         # _deb("_pgdt_pathcb: %s" % f)
         dd, df, tm, sz, orgpath = self._readpathfile(f)
         self._pgdt_alldatafns.add(dd+df)
 
     def _pgdt_datacb(self, datafn):
-        '''Get a datafile name and check that it is referenced by a previously
-        seen pathfile'''
+        """Get a datafile name and check that it is referenced by a previously
+        seen pathfile"""
         p1, fn = os.path.split(datafn)
         p2, dn = os.path.split(p1)
         tst = dn+fn
@@ -347,20 +402,20 @@ class OCRCache(object):
             os.remove(datafn)
 
     def purgedata(self):
-        '''Remove all data files which do not match any from the input list,
+        """Remove all data files which do not match any from the input list,
         based on data contents hash. We make a list of all data files
         referenced by the path files, then walk the data tree,
         removing all unreferenced files. This should only be run after
         an indexing pass, so that the path files are up to date. It's
         a relatively onerous operation as we have to read all the path
-        files, and walk both sets of files.'''
+        files, and walk both sets of files."""
 
         self._pgdt_alldatafns = set()
         self._walk(self.pathdir, self._pgdt_pathcb)
         self._walk(self.objdir, self._pgdt_datacb)
    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import rclconfig
     import getopt
 
