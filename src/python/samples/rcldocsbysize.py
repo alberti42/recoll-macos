9e12c664fe5ae87e5ea11244a050269ae14ee518
Jean-Francois Dockes <jf@dockes.org>
2023-08-20 09:37:12 +0200

    docs by size script

--- src/python/samples/rcldocsbysize.py
+++ src/python/samples/rcldocsbysize.py
@@ -18,20 +18,30 @@
 # Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+#
+# Report the longest documents (by term count), and the biggest directories (by term counts also)
+
 import sys
 import os
 
 import xapian
 import rclconfig
 
+# Number of biggest files we show
+nfilesprinted = 10
+# Depth of paths for which we compute occupation. e.g. /home/me/doc/science would be at depth 4
+dirdepth = 4
+# Number of directories for which we print the totals
+ndirsprinted = 20
+
 def msg(s):
     print(f"{s}", file=sys.stderr)
     
-# Retrieve named value(s) from recoll document data record.
+# Retrieve named value(s) from a recoll document data record.
 # The record format is a sequence of nm=value lines
-def get_attributes(xdb, docid, flds, decode=True):
-    #msg(f"get_attributes docid {docid} flds {flds} decode {decode}")
-    doc = xdb.get_document(docid)
+def get_attributes(xdb, xdocid, flds, decode=True):
+    #msg(f"get_attributes xdocid {xdocid} flds {flds} decode {decode}")
+    doc = xdb.get_document(xdocid)
     data = doc.get_data()
     res = []
     for fld in flds:
@@ -46,7 +56,7 @@ def get_attributes(xdb, docid, flds, decode=True):
                 res.append(data[s+len(fld)+1:e])
     return res
 
-# Not called and does nothing at the moment. Sample code for looking at doc terms in
+# Sample code. Not called and does nothing at the moment. Sample code for looking at doc terms in
 # more detail
 def doc_details(xdb, xdocid):
     #xdoc = xdb.get_document(xdocid)
@@ -76,38 +86,40 @@ if __name__ == '__main__':
         xdocid = int(doc.docid)
         if xdocid % 10000 == 0:
             msg(f"xdocid {xdocid}")
-        alldoclengths.append((doc.doclength, xdocid))
+        try:
+            url = get_attributes(xdb, xdocid, [b"url",], decode=False)[0]
+        except Exception as ex:
+            msg(f"get_attributes failed: {ex}")
+            continue
+        alldoclengths.append((doc.doclength, url))
 
     # Sort by document term count, highest first
     alldoclengths.sort(reverse=True)
 
-    depth = 4
+    # Walk the sorted list, print the "nfilesprinted" longest documents sizes and paths, accumulate
+    # the sizes for directories at depth "dirdepth"
     dirsizes = {}
-    nfilesprinted = 10
     i = 0
     for e in alldoclengths:
-        try:
-            url = get_attributes(xdb, e[1], [b"url",])[0]
-        except:
-            # Can fail because of path decoding error
-            continue
+        path = e[1][7:]
         if i < nfilesprinted:
-            print(f"{e[0]} {url}")
+            print(f"{e[0]} {path}")
             i += 1
-        path = os.path.dirname(url[7:])
+        path = os.path.dirname(path)
         #print(f"PATH {path}")
-        l = path.split("/")
-        if len(l) < depth:
+        l = path.split(b"/")
+        if len(l) < dirdepth:
             dir = path
         else:
-            dir = "/".join(l[0:depth+1])
+            dir = b"/".join(l[0:dirdepth+1])
         #print(f"DIR {dir}")
         if dir in dirsizes:
-            dirsizes[dir] += e[1]
+            dirsizes[dir] += e[0]
         else:
-            dirsizes[dir] = e[1]
+            dirsizes[dir] = e[0]
 
+    # Print the 20 biggest directories
+    print("\nDirectory sizes:")
     dirsizes  = sorted(dirsizes.items(), key = lambda entry: entry[1], reverse = True)
-    for dir, sz in dirsizes[0:20]:
+    for dir, sz in dirsizes[0:ndirsprinted]:
         print(f"{sz} {dir}")
-    
