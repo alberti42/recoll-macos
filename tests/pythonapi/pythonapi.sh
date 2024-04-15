d4ee9c79490870c8e5d6e117da72d3ed9711c9b4
Jean-Francois Dockes <jf@dockes.org>
2024-04-15 15:40:31 +0200

    Removed python2 test

--- tests/pythonapi/pythonapi.sh
+++ tests/pythonapi/pythonapi.sh
@@ -15,14 +15,7 @@ xrun()
 }
 
 (
-    t2=$toptmp/python2out
     t3=$toptmp/python3out
-    for i in *.py;do python2 $i ;done > $t2
-    for i in *.py;do python3 $i ;done > $t3
-
-    if ! cmp $t2 $t3 ; then 
-        echo "Python2 and Python 3 outputs differ: $t2 $t3"
-    fi
     for i in *.py;do xrun python3 $i ;done
 ) 2> $mystderr | egrep -v '^Recoll query: ' > $mystdout
 
