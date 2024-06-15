e0d70a5b100d48dd477a544e8e2008747a8ecfa0
Jean-Francois Dockes <jf@dockes.org>
2024-06-15 14:10:32 +0200

    recoll python extension: rename the now unused .in files and cleanup

--- src/python/recoll/setup-win.py
+++ src/python/recoll/setup-win.py
@@ -1,21 +1,31 @@
+# Windows-specific version of the recoll Python extension package
 from setuptools import setup, Extension
 import os
 import sys
 
-srcdir = "c:/users/bill/documents/recoll/src/python/recoll"
+# For shadow builds: references to the source tree
+pytop = "c:/users/bill/documents/recoll/src/python/recoll"
+# Windows
 recolldeps = "c:/users/bill/documents/recolldeps/msvc"
 
-# The compiled-in values are not used under Windows
-prefix = "c:/install/recoll"
+# Need to set this. Where will the default config be stored. e.g. /usr/share/recoll on linux
+# The compiled-in values are not used under Windows, we search from the binary location, which may not always work, in which case, you will need to set RECOLL_DATADIR in the environment.
 RECOLL_DATADIR = "c:/install/recoll/Share/"
 
 # For shadow builds: references to the source tree
-top = os.path.join(srcdir, '..', '..')
-pytop = srcdir
-localtop = top
-extra_compile_args = ''
+top = os.path.join(pytop, '..', '..')
 
 VERSION = open(os.path.join(top, "RECOLL-VERSION.txt")).read().strip()
+
+# For shadow builds: reference to the top of the local tree (for finding
+# generated .h files, e.g. autoconfig.h)
+localtop = os.path.join(os.path.dirname(__file__), '..', '..')
+
+extra_compile_args = ['-std=c++17']
+define_macros = [
+        ('RECOLL_DATADIR', RECOLL_DATADIR),
+        ('__WIN32__', '1')
+]
 
 include_dirs = [
         os.path.join(top, 'common'),
@@ -25,11 +35,6 @@ include_dirs = [
         os.path.join(top, 'rcldb'), 
         os.path.join(top, 'utils'), 
         os.path.join(top, 'xaposix'), 
-]
-
-define_macros = [
-        ('RECOLL_DATADIR', RECOLL_DATADIR),
-        ('__WIN32__', '1')
 ]
 
 library_dirs = [
@@ -50,16 +55,16 @@ libraries =  ["recoll", "libmagic", "libregex", "libxml2_a", "libxslt_a",
               "psapi", "user32", "kernel32"
 ]
 
-extra_compile_args = ['-std=c++11']
-
 module1 = Extension('_recoll',
                     define_macros = define_macros,
                     include_dirs = include_dirs,
+                    extra_compile_args = extra_compile_args,
                     libraries = libraries,
                     library_dirs = library_dirs,
                     sources = [os.path.join(pytop, 'pyrecoll.cpp'),
                                os.path.join(pytop, 'pyresultstore.cpp'),
-                               os.path.join(pytop, 'pyrclextract.cpp')])
+                               os.path.join(pytop, 'pyrclextract.cpp')
+                               ])
 
 setup (name = 'Recoll',
        version = VERSION,
@@ -71,6 +76,6 @@ setup (name = 'Recoll',
 #       package_dir = {'' : os.path.join(top, 'python', 'recoll')},
        long_description = '''
 ''',
-    packages = ['recoll'],
-    ext_package = 'recoll',
+       packages = ['recoll'],
+       ext_package = 'recoll',
        ext_modules = [module1])
