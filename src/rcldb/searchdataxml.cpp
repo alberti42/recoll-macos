c24a73ff7ee5b5f7c0267c351004fcb1d00397f6
Jean-Francois Dockes <jf@dockes.org>
2024-07-14 09:36:41 +0200

    tabs

--- src/rcldb/searchdataxml.cpp
+++ src/rcldb/searchdataxml.cpp
@@ -54,114 +54,105 @@ string SearchData::asXML()
     ostringstream os;
 
     // Searchdata
-    os << "<SD>" << endl;
+    os << "<SD>" << "\n";
 
     // Clause list
-    os << "<CL>" << endl;
+    os << "<CL>" << "\n";
 
     // List conjunction: default is AND, else print it.
     if (m_tp != SCLT_AND)
-    os << "<CLT>" << tpToString(m_tp) << "</CLT>" << endl;
+        os << "<CLT>" << tpToString(m_tp) << "</CLT>" << "\n";
 
     for (unsigned int i = 0; i <  m_query.size(); i++) {
-    SearchDataClause *c = m_query[i];
-    if (c->getTp() == SCLT_SUB) {
-        LOGERR("SearchData::asXML: can't do subclauses !\n" );
-        continue;
-    }
-    if (c->getTp() == SCLT_PATH) {
-        // Keep these apart, for compat with the older history format. NEG
-        // is ignored here, we have 2 different tags instead.
-        SearchDataClausePath *cl = 
-        dynamic_cast<SearchDataClausePath*>(c);
-        if (cl->getexclude()) {
-        os << "<ND>" << base64_encode(cl->gettext()) << "</ND>" << endl;
+        SearchDataClause *c = m_query[i];
+        if (c->getTp() == SCLT_SUB) {
+            LOGERR("SearchData::asXML: can't do subclauses !\n" );
+            continue;
+        }
+        if (c->getTp() == SCLT_PATH) {
+            // Keep these apart, for compat with the older history format. NEG
+            // is ignored here, we have 2 different tags instead.
+            SearchDataClausePath *cl = dynamic_cast<SearchDataClausePath*>(c);
+            if (cl->getexclude()) {
+                os << "<ND>" << base64_encode(cl->gettext()) << "</ND>" << "\n";
+            } else {
+                os << "<YD>" << base64_encode(cl->gettext()) << "</YD>" << "\n";
+            }
+            continue;
         } else {
-        os << "<YD>" << base64_encode(cl->gettext()) << "</YD>" << endl;
-        }
-        continue;
-    } else {
 
-        os << "<C>" << endl;
+            os << "<C>" << "\n";
 
-        if (c->getexclude())
-        os << "<NEG/>" << endl;
+            if (c->getexclude())
+                os << "<NEG/>" << "\n";
 
-        if (c->getTp() != SCLT_AND) {
-        os << "<CT>" << tpToString(c->getTp()) << "</CT>" << endl;
-        }
-        if (c->getTp() == SCLT_FILENAME) {
-        SearchDataClauseFilename *cl = 
-            dynamic_cast<SearchDataClauseFilename*>(c);
-        os << "<T>" << base64_encode(cl->gettext()) << "</T>" << endl;
-        } else {
-        SearchDataClauseSimple *cl = 
-            dynamic_cast<SearchDataClauseSimple*>(c);
-        if (!cl->getfield().empty()) {
-            os << "<F>" << base64_encode(cl->getfield()) << "</F>" << 
-            endl;
-        }
-        os << "<T>" << base64_encode(cl->gettext()) << "</T>" << endl;
-        if (cl->getTp() == SCLT_RANGE) {
-                    SearchDataClauseRange *clr =
-            dynamic_cast<SearchDataClauseRange*>(cl);
+            if (c->getTp() != SCLT_AND) {
+                os << "<CT>" << tpToString(c->getTp()) << "</CT>" << "\n";
+            }
+            if (c->getTp() == SCLT_FILENAME) {
+                SearchDataClauseFilename *cl = dynamic_cast<SearchDataClauseFilename*>(c);
+                os << "<T>" << base64_encode(cl->gettext()) << "</T>" << "\n";
+            } else {
+                SearchDataClauseSimple *cl = dynamic_cast<SearchDataClauseSimple*>(c);
+                if (!cl->getfield().empty()) {
+                    os << "<F>" << base64_encode(cl->getfield()) << "</F>" << "\n";
+                }
+                os << "<T>" << base64_encode(cl->gettext()) << "</T>" << "\n";
+                if (cl->getTp() == SCLT_RANGE) {
+                    SearchDataClauseRange *clr = dynamic_cast<SearchDataClauseRange*>(cl);
                     const string& t = clr->gettext2();
                     if (!t.empty()) {
-                        os << "<T2>" << base64_encode(clr->gettext2()) <<
-                            "</T2>" << endl;
+                        os << "<T2>" << base64_encode(clr->gettext2()) << "</T2>" << "\n";
                     }
                 }
-        if (cl->getTp() == SCLT_NEAR || cl->getTp() == SCLT_PHRASE) {
-            SearchDataClauseDist *cld = 
-            dynamic_cast<SearchDataClauseDist*>(cl);
-            os << "<S>" << cld->getslack() << "</S>" << endl;
+                if (cl->getTp() == SCLT_NEAR || cl->getTp() == SCLT_PHRASE) {
+                    SearchDataClauseDist *cld = dynamic_cast<SearchDataClauseDist*>(cl);
+                    os << "<S>" << cld->getslack() << "</S>" << "\n";
+                }
+            }
+            os << "</C>" << "\n";
         }
-        }
-        os << "</C>" << endl;
     }
-    }
-    os << "</CL>" << endl;
+    os << "</CL>" << "\n";
 
     if (m_haveDates) {
-    if (m_dates.y1 > 0) {
-        os << "<DMI>" << 
-        "<D>" << m_dates.d1 << "</D>" <<
-        "<M>" << m_dates.m1 << "</M>" << 
-        "<Y>" << m_dates.y1 << "</Y>" 
-           << "</DMI>" << endl;
-    }
-    if (m_dates.y2 > 0) {
-        os << "<DMA>" << 
-        "<D>" << m_dates.d2 << "</D>" <<
-        "<M>" << m_dates.m2 << "</M>" << 
-        "<Y>" << m_dates.y2 << "</Y>" 
-           << "</DMA>" << endl;
-    }
+        if (m_dates.y1 > 0) {
+            os << "<DMI>" << 
+                "<D>" << m_dates.d1 << "</D>" <<
+                "<M>" << m_dates.m1 << "</M>" << 
+                "<Y>" << m_dates.y1 << "</Y>" 
+               << "</DMI>" << "\n";
+        }
+        if (m_dates.y2 > 0) {
+            os << "<DMA>" << 
+                "<D>" << m_dates.d2 << "</D>" <<
+                "<M>" << m_dates.m2 << "</M>" << 
+                "<Y>" << m_dates.y2 << "</Y>" 
+               << "</DMA>" << "\n";
+        }
     }
 
     if (m_minSize != -1) {
-    os << "<MIS>" << m_minSize << "</MIS>" << endl;
+        os << "<MIS>" << m_minSize << "</MIS>" << "\n";
     }
     if (m_maxSize != -1) {
-    os << "<MAS>" << m_maxSize << "</MAS>" << endl;
+        os << "<MAS>" << m_maxSize << "</MAS>" << "\n";
     }
 
     if (!m_filetypes.empty()) {
-    os << "<ST>";
-    for (vector<string>::iterator it = m_filetypes.begin(); 
-         it != m_filetypes.end(); it++) {
-        os << *it << " ";
-    }
-    os << "</ST>" << endl;
+        os << "<ST>";
+        for (const auto& ft : m_filetypes) {
+            os << ft << " ";
+        }
+        os << "</ST>" << "\n";
     }
 
     if (!m_nfiletypes.empty()) {
-    os << "<IT>";
-    for (vector<string>::iterator it = m_nfiletypes.begin(); 
-         it != m_nfiletypes.end(); it++) {
-        os << *it << " ";
-    }
-    os << "</IT>" << endl;
+        os << "<IT>";
+        for (const auto& nft : m_nfiletypes) {
+            os << nft << " ";
+        }
+        os << "</IT>" << "\n";
     }
 
     os << "</SD>";
