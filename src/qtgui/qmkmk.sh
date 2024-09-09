4fe04d04b85b916194b89ea7ac8558688570dd59
Jean-Francois Dockes <jf@dockes.org>
2024-09-09 19:43:01 +0200

    minor build and warning fixes for freebsd

--- src/qtgui/qmkmk.sh
+++ src/qtgui/qmkmk.sh
@@ -13,12 +13,15 @@ QMAKE=${QMAKE:-qmake}
 MAKE=${MAKE:-make}
 
 ncpus=2
-if test -x /usr/bin/nproc ; then
+if which nproc > /dev/null ; then
     ncpus=`nproc`
 elif which sysctl > /dev/null ; then
-    ncpus=`sysctl hw.ncpu | awk '{print $3}'`
+    ncpus=`sysctl hw.ncpu | awk '{print $2}'`
 elif test -f /proc/cpuinfo ; then
     ncpus=`grep -E '^processor[        ]*:' /proc/cpuinfo | wc -l`
+fi
+if test -z "$ncpus" ; then
+    ncpus=1
 fi
 
 cd $dir
